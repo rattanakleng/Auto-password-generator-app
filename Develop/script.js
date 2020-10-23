886d42e379868aca1e0de85541e0af9b1e5b0bf6
Rattanak Leng <rleng@live.com>
2020-10-23 14:09:44 -0700

    Update JS code with new solution

--- Develop/script.js
+++ Develop/script.js
@@ -8,133 +8,94 @@ function generatePassword() {
     // Prompted for password criteria
     // THEN I select which criteria to include in the password
     // WHEN prompted for the length of the password with total charactor between 8 and 128 
-    // Create var to store password length in prompt
 
-    // var pwLength = prompt ("Please enter number for the lenght of password you want to created");
-    // var numCheck = isNaN(pwLength);
-
-    // Create function to verify the input number is between 8 and 128 and covert string to interger
+    // TO DO
+    // Create VAR store data
     var pwLength;
     var numCheck;
+    var lowerCase;
+    var lowerCasePw = "abcdefghijklmnopqrstuvwxyz";
+    var upperCase;
+    var upperCasePw = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var number;
+    var numberPw = "0123456789";
+    var speChar;
+    var specCharPw = "!&%$#@()^";
+    var newPw = "";
+    var returnNewPw = "";
 
-    function lengthCheck() {
+    //FUNCTION checkLength to PROMPT in put password length. CHECK input type and number 8-128 
+    function checkLenth() {
         pwLength = prompt("Please enter any numbers between 8 and 128 for the lenght of password you want to created");
+
         numCheck = isNaN(pwLength);
-        if (numCheck == true) {
+        if (numCheck == true || pwLength < 8 || pwLength > 128) {
             alert("Please enter number between 8 and 128 for password length.");
             pwLength = prompt("Please enter any numbers between 8 and 128 for the lenght of password you want to created");
+            checkLenth();
         }
     }
 
-    lengthCheck();
-
-    function lengthCheck2() {
-        pwLength = prompt("Please enter any numbers between 8 and 128 only!");
-        numCheck = isNaN(pwLength);
-        if (numCheck == true) {
-            alert("Please enter number only");
-            pwLength = prompt("Please enter any numbers between 8 and 128 for the lenght of password you want to created");
-        }
-
-        inputChar();
-    }
-
-    // Verify if the passworkd lenght is between 8 and 128
-    var lowerCase;
-    var upperCase;
-    var number;
-    var speChar;
-
-    function inputChar() {
+    //FUNCTION CONFIRM and collect password charactor for password
+    function checkCharatorType() {
         if (pwLength >= 8 && pwLength <= 128) {
 
-            // After verify the lenght of at least 8 characters and no more than 128 characters
-            // Alert character types to include in the password
-            // Connfirm for lowercase, create varible store user respond
+            // CONFIRM for lowercase and store user respond
             lowerCase = confirm("Do you want your password contain lowercase letter?");
+            if (lowerCase == true) {
+                newPw = lowerCasePw;
+                console.log(newPw);
+            };
 
-
-            // Confirm for uppercase and create varible store user respond
+            // CONFIRM for uppercase and create varible store user respond
             upperCase = confirm("Do you want your password contain uppercase letter?");
+            if (upperCase == true) {
+                newPw = newPw + upperCasePw;
+                console.log(newPw);
+            };
 
-            // Confirm for numeric and create varible store user respond
+            // CONFIRM for numeric and create varible store user respond
             number = confirm("Do you want your password contain number?");
+            if (number == true) {
+                newPw = newPw + numberPw;
+                console.log(newPw);
+            };
 
-            // Confirm for special characters create varible store user respond
+            // CONFIRM for special characters create varible store user respond
             speChar = confirm("Do you want your password contain special charactor?");
+            if (speChar == true) {
+                newPw = newPw + specCharPw;
+                console.log(newPw);
+            };
+
+            // Check if at lease one password option is sleclected
+            if (lowerCase == false && upperCase == false && number == false && speChar == false) {
+                alert("Please include letter, number, and special charactor for your password!")
+                checkCharatorType();
+            };
+
+            for (var i = 0; i <= pwLength; i++) {
+                returnNewPw += newPw.charAt(Math.floor(Math.random() * newPw.length));
+            };
+            console.log(returnNewPw);
+            return returnNewPw;
+
 
         } else {
-            lengthCheck2();
+            inputCheck();
         }
     }
 
-    inputChar();
-    console.log(lowerCase);
-    console.log(upperCase);
-    console.log(number);
-    console.log(speChar);
+    // Call back function to run code and return value that matches the selected criteria
+    checkLenth();
+    checkCharatorType();
 
-    // THEN my confirm if input should be validated and at least one character type should be selected
-    // WHEN all prompts are answered
-    if (lowerCase == false && upperCase == false && number == false && speChar == false) {
-        alert("Please include letter, number, and special charactor for your password!")
-        inputChar();
-
-    } else if (lowerCase == true && upperCase == false && number == false && speChar == false) {
-        alert("Your password contains lowercase letters only");
-        //return random lowercase letters only within the input lenght
-        var lowerCasePw = "";
-        var possible = "abcdefghijklmnopqrstuvwxyz";
-
-        function returnLowercasePw() {
-            for (var i = 0; i <= pwLength; i++) {
-                lowerCasePw += possible.charAt(Math.floor(Math.random() * possible.length));
-            }
-            return lowerCasePw;
-        }
-        returnLowercasePw()
-        //alert(lowerCasePw);
-        return lowerCasePw;
-
-
-    } else if (lowerCase == false && upperCase == true && number == false && speChar == false) {
-        confirm("Your password contains uppercase letters only");
-    } else if (lowerCase == false && upperCase == false && number == true && speChar == false) {
-        confirm("Your password contains numbers only");
-    } else if (lowerCase == false && upperCase == false && number == false && speChar == true) {
-        confirm("Your password contains special charactors only");
-    } else if (lowerCase == true && upperCase == true && number == false && speChar == false) {
-        confirm("Your password contains lowercase and uppercase letters only");
-    } else if (lowerCase == true && upperCase == true && number == true && speChar == false) {
-        confirm("Your password contains letters and numbers only");
-    } else if (lowerCase == true && upperCase == true && number == false && speChar == true) {
-        confirm("Your password contains letters and special charactor only");
-    } else if (lowerCase == true && upperCase == false && number == true && speChar == false) {
-        confirm("Your password contains lowercase letters and numbers only");
-    } else if (lowerCase == true && upperCase == true && number == true && speChar == true) {
-        confirm("Your password contains lowercase letter, numbers, special charactor only");
-    } else if (lowerCase == false && upperCase == true && number == true && speChar == false) {
-        confirm("Your password contains uppercase letters and numbers only");
-    }
-    else {
-        confirm("Your password contains letters, numbers, and special characters");
-    }
-
-    // THEN a password is generated that matches the selected criteria
-
-
-
-    // WHEN the password is generated
-    // THEN the password is either displayed in an alert or written to the page
-
-    //return lastPassword;
-
-
+    return returnNewPw;
 }
 
 
-
-
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -144,26 +105,7 @@ function writePassword() {
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
-
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
