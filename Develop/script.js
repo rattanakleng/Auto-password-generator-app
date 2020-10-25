69d10b1b36dbdd7cd11120cf73daa9dabec6b34c
Rattanak Leng <rleng@live.com>
2020-10-24 17:26:44 -0700

    update javascript - fix all false condition

--- Develop/script.js
+++ Develop/script.js
@@ -11,8 +11,23 @@ function generatePassword() {
 
     // TO DO
     // Create VAR store data
+    // var pwLength;
+    // var numCheck;
+    // var lowerCase;
+    // var lowerCasePw = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    // var upperCase;
+    // var upperCasePw = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    // var number;
+    // var numberPw = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    // var speChar;
+    // var speCharPw = ["!", "&", "%", "$", "#", "@", "(", ")", "^"];
+    // var newPw = "";
+    // var returnNewPw = "";
+
+
+
+
     var pwLength;
-    var numCheck;
     var lowerCase;
     var lowerCasePw = "abcdefghijklmnopqrstuvwxyz";
     var upperCase;
@@ -20,78 +35,90 @@ function generatePassword() {
     var number;
     var numberPw = "0123456789";
     var speChar;
-    var specCharPw = "!&%$#@()^";
+    var speCharPw = "!&%$#@()^";
     var newPw = "";
     var returnNewPw = "";
+    var finalPassword = "";
+
 
     //FUNCTION checkLength to PROMPT in put password length. CHECK input type and number 8-128 
-    function checkLenth() {
-        pwLength = prompt("Please enter any numbers between 8 and 128 for the lenght of password you want to created");
 
-        numCheck = isNaN(pwLength);
-        if (numCheck == true || pwLength < 8 || pwLength > 128) {
-            alert("Please enter number between 8 and 128 for password length.");
-            pwLength = prompt("Please enter any numbers between 8 and 128 for the lenght of password you want to created");
-            checkLenth();
-        }
+    pwLength = prompt("Please enter any numbers between 8 and 128 for the lenght of password you want to creat.");
+
+
+    while (isNaN(pwLength) || pwLength < 8 || pwLength > 128) {
+        //pwLength = prompt("Please enter any numbers between 8 and 128 for the lenght of password you want to create.");
+        console.log("try again");
+        pwLength = prompt("Please enter any numbers between 8 and 128 for the lenght of password you want to creat.");
+
     }
+
+    lowerCase = confirm("Do you want your password contain lowercase letter?");
+    upperCase = confirm("Do you want your password contain uppercase letter?");
+    number = confirm("Do you want your password contain number?");
+    speChar = confirm("Do you want your password contain special charactor?");
+
+    if (lowerCase === false && upperCase === false && number === false && speChar === false) {
+        alert("Password must include letter, number, or special charactor for your password! Please try gain")
+        // pwLength = prompt("Please enter any numbers between 8 and 128 for the lenght of password you want to creat.");
+        // //generatePassword();
+        return "Click Generate Password botton to create a password";
+    };
 
     //FUNCTION CONFIRM and collect password charactor for password
-    function checkCharatorType() {
-        if (pwLength >= 8 && pwLength <= 128) {
+    while (newPw.length < pwLength) {
 
-            // CONFIRM for lowercase and store user respond
-            lowerCase = confirm("Do you want your password contain lowercase letter?");
-            if (lowerCase == true) {
-                newPw = lowerCasePw;
-                console.log(newPw);
-            };
+        // CONFIRM for lowercase and store user respond
+        //lowerCase = confirm("Do you want your password contain lowercase letter?");
+        if (lowerCase) {
 
-            // CONFIRM for uppercase and create varible store user respond
-            upperCase = confirm("Do you want your password contain uppercase letter?");
-            if (upperCase == true) {
-                newPw = newPw + upperCasePw;
-                console.log(newPw);
-            };
+            newPw = lowerCasePw.charAt(Math.floor(Math.random() * lowerCasePw.length));
 
-            // CONFIRM for numeric and create varible store user respond
-            number = confirm("Do you want your password contain number?");
-            if (number == true) {
-                newPw = newPw + numberPw;
-                console.log(newPw);
-            };
+            //newPw = lowerCasePw;
+            console.log(newPw);
 
-            // CONFIRM for special characters create varible store user respond
-            speChar = confirm("Do you want your password contain special charactor?");
-            if (speChar == true) {
-                newPw = newPw + specCharPw;
-                console.log(newPw);
-            };
+            //returnNewPw += newPw.charAt(Math.floor(Math.random() * newPw.length))
+        };
 
-            // Check if at lease one password option is sleclected
-            if (lowerCase == false && upperCase == false && number == false && speChar == false) {
-                alert("Please include letter, number, and special charactor for your password!")
-                checkCharatorType();
-            };
+        // CONFIRM for uppercase and create varible store user respond
+        //upperCase = confirm("Do you want your password contain uppercase letter?");
+        if (upperCase) {
 
-            for (var i = 0; i <= pwLength; i++) {
-                returnNewPw += newPw.charAt(Math.floor(Math.random() * newPw.length));
-            };
-            console.log(returnNewPw);
-            return returnNewPw;
+            newPw = newPw + upperCasePw.charAt(Math.floor(Math.random() * upperCasePw.length));
+            console.log(newPw);
+        };
 
+        // CONFIRM for numeric and create varible store user respond
+        // number = confirm("Do you want your password contain number?");
+        if (number) {
+            newPw = newPw + numberPw.charAt(Math.floor(Math.random() * numberPw.length));
+            console.log(newPw);
+        };
 
-        } else {
-            inputCheck();
-        }
+        // CONFIRM for special characters create varible store user respond
+        //speChar = confirm("Do you want your password contain special charactor?");
+        if (speChar) {
+            newPw = newPw + speCharPw.charAt(Math.floor(Math.random() * speCharPw.length));
+            console.log(newPw);
+        };
+
+        // finalPassword = newPw
+
+        finalPassword = newPw;
+        for (var i = 0; i < pwLength; i++) {
+            returnNewPw += newPw.charAt(Math.floor(Math.random() * newPw.length));
+        };
+
+        return returnNewPw;
     }
 
-    // Call back function to run code and return value that matches the selected criteria
-    checkLenth();
-    checkCharatorType();
-
     return returnNewPw;
+
 }
+
+// Call back function to run code and return value that matches the selected criteria
+// console.log(returnNewPw);
+// return finalPassword;
 
 
 // WHEN the password is generated
